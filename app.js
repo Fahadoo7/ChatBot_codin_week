113ac4b3fa41bfe3a317ece18fa4170102a81467
Syed Fahad <57718052+Fahadoo7@users.noreply.github.com>
2021-10-24 13:13:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,9 @@
 
 
 var date = new Date();
-var days = {0:'Monday',1:'Tuesday',2:'Wednesday',3:'Thursday',4:'Friday',5:'Saturday',6:'Sunday'}
+var days = {0:'Sunday',1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday'}
 var x = document.getElementById("tsk1")
-var dayIndex = Number(date.getDay())-1;
-
+var dayIndex = Number(date.getDay());
 
 
 if(date.getHours()>12){
